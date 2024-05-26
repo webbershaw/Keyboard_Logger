466b3c2a9504de80c8930306d0fc39aaec51ba94
WebberShaw <webber@tech-webs.com>
2024-05-26 20:47:22 +0800

    bugs fixed

--- client/winstart.bat
+++ client/winstart.bat
@@ -1,18 +1,18 @@
 @echo off
-REM 保存当前目录
+
 set CURRENT_DIR=%cd%
 
-REM 切换到上一级目录
+
 cd ..
 
-REM 激活虚拟环境
+
 call winkbvenv\Scripts\activate
 
-REM 返回到原始目录
+
 cd %CURRENT_DIR%
 
-REM 运行main.py
+
 python main.py
 
-REM 保持命令行窗口打开，以便查看输出结果
-@REM pause
+
+pause
